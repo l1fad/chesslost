3f36a6d5635242038f10ca1725a7241e39b0e48d
Иванов Алексей Анатольевич <ip711s09@mwp05.csc.local>
2018-04-24 12:48:23 +0700

    Displaying of chessdesk through the massive

--- chess.c
+++ chess.c
@@ -2,6 +2,58 @@
 
  
 void main() {
-printf("8 r n b q k b n r\n7 p p p p p p p p\n6\n5\n4\n3\n2 P P P P P P P P\n1 R N B Q K B N R\n  a b c d e f g h \n");
 
+char a[8][8];
+int i,j;
+a[0][0]='r'; 
+a[0][7]='r'; 
+
+a[0][1]='n';
+a[0][6]='n'; 
+
+a[0][2]='b';
+a[0][5]='b'; 
+
+a[0][3]='q'; 
+
+a[0][4]='k'; 
+
+
+
+a[7][0]='R'; 
+a[7][7]='R'; 
+
+a[7][1]='N';
+a[7][6]='N'; 
+
+a[7][2]='B';
+a[7][5]='B'; 
+
+a[7][3]='Q'; 
+
+a[7][4]='K'; 
+for (i=0;i<8;i++) 
+{
+	a[1][i]='p';
+	a[6][i]='P';
+}
+for (i=2;i<6;i++) 
+{	
+	for (j=0;j<8;j++)
+	{
+		a[i][j]=' ';
+	}
+}
+for (i=0;i<8;i++) 
+{	printf("%d ",8-i);
+	for (j=0;j<8;j++)
+	{
+		printf("%c",a[i][j]);
+	}
+	printf("\n");
+}
+printf("  ");
+for (i=0;i<8;i++)
+	printf("%d",1+i);
+printf("\n");
 } 
